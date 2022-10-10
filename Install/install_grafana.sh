f089c4aa72cef09820ec7c4dcfa32d3172579f6c
michalrothanzl <michal.rothanzl@stratox.cz>
2022-10-10 11:51:31 +0200

    Grafana install

--- Install/install_grafana.sh
+++ Install/install_grafana.sh
@@ -3,10 +3,11 @@ sudo apt-get install -y software-properties-common wget
 wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
 
 echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
-echo "deb https://packages.grafana.com/oss/deb beta main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
 
-sudo apt-get update
-sudo apt-get install grafana
+sudo apt-get -y update
+sudo apt-get -y install grafana
+
+sudo chown -R grafana:grafana /usr/sbin/grafana-server
 
 sudo systemctl daemon-reload
 sudo systemctl start grafana-server
