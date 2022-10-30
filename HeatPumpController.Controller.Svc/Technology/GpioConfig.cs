3ed981e9f5d5d6057527936b0dce81a38b9c7d3f
michalrothanzl <michal.rothanzl@stratox.cz>
2022-10-30 13:55:37 +0100

    Add extra heating

--- HeatPumpController.Controller.Svc/Technology/GpioConfig.cs
+++ HeatPumpController.Controller.Svc/Technology/GpioConfig.cs
@@ -13,14 +13,14 @@ public class GpioConfig
         public const int HandleHeatPump = 14;
         public const int HandleUpperValve = 15;
         public const int HandleLowerValve = 18;
+        public const int HandleExtraHeating = 23;
         
-        public const int HandleHeatingCircuitBathRoomWall = 23;
         public const int HandleHeatingCircuitBathRoom = 24;
         public const int HandleHeatingCircuitSmallRoom = 25;
         public const int HandleHeatingCircuitBedRoom = 8;
         public const int HandleHeatingCircuitKitchen = 7;
         public const int HandleHeatingCircuitLivingRoom = 1;
-
-        public const int HandleExtraHeating = 12;
+        public const int HandleHeatingCircuitBathRoomWall = 12;
+        
     }
 }
