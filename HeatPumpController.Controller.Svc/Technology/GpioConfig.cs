fa09ee29fafbcf1b71e0fee3b9bfb576d855c709
michalrothanzl <michal.rothanzl@stratox.cz>
2022-10-31 11:26:48 +0100

    Extend ManualHandling.razor

--- HeatPumpController.Controller.Svc/Technology/GpioConfig.cs
+++ HeatPumpController.Controller.Svc/Technology/GpioConfig.cs
@@ -15,12 +15,12 @@ public class GpioConfig
         public const int HandleLowerValve = 18;
         public const int HandleExtraHeating = 23;
         
-        public const int HandleHeatingCircuitBathRoom = 24;
-        public const int HandleHeatingCircuitSmallRoom = 25;
-        public const int HandleHeatingCircuitBedRoom = 8;
-        public const int HandleHeatingCircuitKitchen = 7;
+        public const int HandleHeatingCircuitBathRoom = 21;
+        public const int HandleHeatingCircuitSmallRoom = 20;
+        public const int HandleHeatingCircuitBedRoom = 16;
+        public const int HandleHeatingCircuitKitchen = 12;
         public const int HandleHeatingCircuitLivingRoom = 1;
-        public const int HandleHeatingCircuitBathRoomWall = 12;
+        public const int HandleHeatingCircuitBathRoomWall = 7;
         
     }
 }
